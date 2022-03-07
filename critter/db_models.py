6deba2871ef082fcd2c995a9f48a4c36b5c59792
Christopher John <christopherjohn00@protonmail.com>
2022-03-07 15:39:04 -0500

    Removed unneeded imports, reformatted model property names, added explanitory notation, removed initialization functions, and added password hash related functions to Users model.

--- critter/db_models.py
+++ critter/db_models.py
@@ -1,131 +1,98 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base 
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import backref
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, LoginManager
 
-from critter import db
+db = SQLAlchemy()
+login = LoginManager()
 
-# Manage tables
-
-class Users(db.Model):
+class Users(UserMixin, db.Model):
 
 	__tablename__ = "Users"
 
-	UserID = db.Column(db.Integer, primary_key = True)
-	Username = db.Column(db.String)
-	Email = db.Column(db.String)
-	Password = db.Column(db.String)
+	id = db.Column(db.Integer, primary_key = True)		#User ID
+	username = db.Column(db.String(), unique = True)	#Username
+	email = db.Column(db.String(), unique = True)		#Email
+	password_hash = db.Column(db.String())				#Password Hash
 	
 	# Create the relationship between Users and User_Profiles
 	user_profiles = db.relationship('User_Profiles', backref='users', uselist=False)
 
-	
-	def __init__(self, UserID, Username, Email, Password):
-		self.UserID = UserID
-		self.Username = Username
-		self.Email = Email
-		self.Password = Password
+	#Creates a password hash from a given password.
+	def set_password(self,password):
+		self.password_hash = generate_password_hash(password)
+     
+	#Checks if the given password matches the existing password hash.
+	def check_password(self,password):
+		return check_password_hash(self.password_hash,password)
+		
 		
 class User_Profiles(db.Model):
 
 	__tablename__ = "User_Profiles"
 	
-	UserID = db.Column(db.Integer, db.ForeignKey('Users.UserID'), primary_key = True)
-	Username = db.Column(db.String)
-	Gender = db.Column(db.String)
-	Bio = db.Column (db.String)
-	Avatar = db.Column(db.LargeBinary)
+	id = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key = True)	#User ID
+	username = db.Column(db.String)												#Username
+	gender = db.Column(db.String)												#Gender
+	bio = db.Column (db.String)													#Bio Text
+	avatar = db.Column(db.LargeBinary)											#Avater ID (Media ID)
 	
 	# Create relationships
-	#posts = db.relationship('Posts', backref='user_profiles')
+	# posts = db.relationship('Posts', backref='user_profiles')
 	# #follows = relationship('Follows', backref='user_profiles')
 	# #likes = relationship('Likes', backref='user_profiles')
-	
-	def __init__(self, UserID, Username, Gender, Bio, Avatar):
-		self.UserID = UserID
-		self.Username = Username
-		Self.Gender = Gender
-		self.Bio = Bio
-		self.Avatar = Avatar
 
 class Media(db.Model):
 	
 	__tablename__ = 'Media'
 	
-	MediaID = db.Column(db.Integer, primary_key = True)
-	URI = db.Column(db.String)
+	id = db.Column(db.Integer, primary_key = True)	#Media ID
+	uri = db.Column(db.String)						#Media URI
 	
 	# Create Relationships
 	posts = db.relationship('Posts', backref='media')
-	
-	def __init__(self, MediaID, URI):
-		self.MediaID = MediaID
-		self.URI = URI
 
 class Post_Types(db.Model):
 
 	__tablename__ = 'Post_Types'
 	
-	TypeID = db.Column(db.Integer, primary_key = True)
-	URI = db.Column(db.String)
+	id = db.Column(db.Integer, primary_key = True)	#Post Type ID
+	type = db.Column(db.String)						#Post Type [ text | photo | video ]
 	
 	# Create relationships
 	posts = db.relationship('Posts', backref='types')
-	
-	def __init__(self, TypeID, URI):
-		self.TypeID = TypeID
-		self.URI = URI
 
 class Posts(db.Model):
 	
 	__tablename__ = 'Posts'
 	
-	PostID = db.Column(db.Integer, primary_key = True)
-	UserID = db.Column(db.Integer, db.ForeignKey(User_Profiles.UserID))
-	PostType = db.Column(db.Integer, db.ForeignKey(Post_Types.TypeID))
-	Text = db.Column(db.String)
-	MediaID = db.Column(db.Integer, db.ForeignKey(Media.MediaID))
-	PostTime = db.Column(db.DateTime)
+	id = db.Column(db.Integer, primary_key = True)					#Post ID
+	uid = db.Column(db.Integer, db.ForeignKey(User_Profiles.id))	#Author ID
+	type = db.Column(db.Integer, db.ForeignKey(Post_Types.type))	#Post Type (Type ID)
+	text = db.Column(db.String)										#Text-Content
+	mid = db.Column(db.Integer, db.ForeignKey(Media.id))			#Media ID
+	time = db.Column(db.DateTime)									#Date/time of post
 	
 	# Create relationships
 	likes = db.relationship('Likes', backref='postslikes')
-	
-	def __init__(self, PostID, UserID, PostType, Text, MediaID, PostTime):
-		self.PostID = PostID
-		self.UserID = UserID
-		self.PostType = PostType
-		self.Text = Text
-		self.MediaID = MediaID
-		self.PostTime = PostTime
+
 		
 class Likes(db.Model):
 	
 	__tablename__ = 'Likes'
 	
-	LikeID = db.Column(db.Integer, primary_key = True)
-	UserID = db.Column(db.Integer, db.ForeignKey(User_Profiles.UserID))
-	PostID = db.Column (db.Integer, db.ForeignKey(Posts.PostID))
-	
-	def __init__(self, LikeID, UserID, PostID):
-		self.LikeID = LikeID
-		self.UserID = UserID
-		self.PostID = PostID
+	id = db.Column(db.Integer, primary_key = True)					#Like ID
+	uid = db.Column(db.Integer, db.ForeignKey(User_Profiles.id))	#Liker's ID
+	pid = db.Column (db.Integer, db.ForeignKey(Posts.id))			#Liked Post ID
+
 		
 class Follows(db.Model):
 
 	__tablename__ = 'Follows'
 	
-	RelationshipID = db.Column(db.Integer, primary_key = True)
-	UserID = db.Column(db.Integer, db.ForeignKey(User_Profiles.UserID))
-	FollowingID = Column(db.Integer, db.ForeignKey(User_Profiles.UserID))
-	
-	def __init__(self, RelationshipID, UserID, FollowingID):
-		self.RelationshipID = RelationshipID
-		self.UserID = UserID
-		self.FollowingID = FollowingID
+	id = db.Column(db.Integer, primary_key = True)					#Relationship ID
+	uid = db.Column(db.Integer, db.ForeignKey(User_Profiles.id))	#Follower's ID
+	fid = db.Column(db.Integer, db.ForeignKey(User_Profiles.id))	#Followed's ID
+
 
 
 
