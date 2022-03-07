e3d927647e2eecdcba0ce5864b11b2217aafadc9
Christopher John <christopherjohn00@protonmail.com>
2022-03-07 17:25:27 -0500

    Updated formating and removed unneeded imports.

--- critter/db_queries.py
+++ critter/db_queries.py
@@ -1,123 +1,105 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base 
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import backref
 
-from critter import db
-from critter import db_models as model
-
-
-
+import db_models
+from db_models import db
 
 # GET data methods
 def getUserLoginInfoByID(id):
-	result = db.session.query(model.Users).filter(model.Users.UserID == id).first()
+	result = db.session.query(db_models.Users).filter(db_models.Users.id == id).first()
+
 	if result is not None:
-		return result.Username
+		info = {
+			"id": result.id,
+			"username": result.username,
+			"email": result.email,
+			"password_hash": result.password_hash
+		}
+		return info
 	else:
-		return "-1"
+		return None
 
 # Returns dictionary with user login info
 def getUserLoginInfoByUsername(username):
-	result = db.session.query(model.Users).filter(model.Users.Username == username).first()
+	result = db.session.query(db_models.Users).filter(db_models.Users.Username == username).first()
 	
 	if result is not None:
 		info = {
-			"UserID": result.UserID,
-			"Username": result.Username,
-			"Email": result.Email,
-			"Password": result.Password
+			"id": result.id,
+			"username": result.username,
+			"email": result.email,
+			"password_hash": result.password_hash
 		}
 		return info
 	else:
-		return "-1"
+		return None
 	
 # Returns dictionary with user profile info		
 def getUserProfileInfoByID(id):
-	result = db.session.query(model.User_Profiles).filter(model.User_Profiles.UserID == id).first()
+	result = db.session.query(db_models.User_Profiles).filter(db_models.User_Profiles.id == id).first()
 	if result is not None:
 		info = {
-			"UserID": result.UserID,
-			"Username": result.Username,
-			"Gender": result.Gender,
-			"Bio": result.Bio
+			"id": result.id,
+			"username": result.username,
+			"gender": result.gender,
+			"bio": result.bio
 			#"Avatar": result.Avatar
 		}
 		return info
 	else:
-		return "-1"
+		return None
 	
 # Returns dictionary with user profile info	
 def getUserProfileInfoByUsername(username):
-	result = db.session.query(model.User_Profiles).filter(model.User_Profiles.Username == username).first()
+	result = db.session.query(db_models.User_Profiles).filter(db_models.User_Profiles.username == username).first()
 	if result is not None:
 		info = {
-			"UserID": result.UserID,
-			"Username": result.Username,
-			"Gender": result.Gender,
-			"Bio": result.Bio
+			"id": result.id,
+			"username": result.username,
+			"gender": result.gender,
+			"bio": result.bio
 			#"Avatar": result.Avatar
 		}
 		return info
 	else:
-		return "-1"
+		return None
 
 # Returns a dictionary with post info
 def getPostByPostID(id):
-	result = db.session.query(model.Posts).filter(model.Posts.PostID == id).first()
+	result = db.session.query(db_models.Posts).filter(db_models.Posts.id == id).first()
 	if result is not None:
 		info = {
-			"PostID": result.PostID,
-			"UserID": result.UserID,
-			"PostType": result.PostType,
-			"Text": result.Text,
-			"MediaID": result.MediaID,
-			"PostTime": result.PostTime
+			"id": result.id,
+			"uid": result.uid,
+			"type": result.type,
+			"text": result.text,
+			"mid": result.mid,
+			"time": result.time
 		}
 		return info
 	else:
-		info = {
-			"PostID": "Not found",
-			"UserID": "Not found",
-			"PostType": "Not found",
-			"Text": "Not found",
-			"MediaID": "Not found",
-			"PostTime": "Not found"
-		}
-		return info
+		return None
 
 # Returns the *most recent* post by the user
 def getPostByUserID(id):
-	result = db.session.query(model.Posts).filter(model.Posts.UserID == id).first()
+	result = db.session.query(db_models.Posts).filter(db_models.Posts.uid == id).first()
 	if result is not None:
 		info = {
-			"PostID": result.PostID,
-			"UserID": result.UserID,
-			"PostType": result.PostType,
-			"Text": result.Text,
-			"MediaID": result.MediaID,
-			"PostTime": result.PostTime
+			"id": result.id,
+			"uid": result.uid,
+			"type": result.type,
+			"text": result.text,
+			"mid": result.mid,
+			"time": result.time
 		}
 		return info
 	else:
-		info = {
-			"PostID": "Not found",
-			"UserID": "Not found",
-			"PostType": "Not found",
-			"Text": "Not found",
-			"MediaID": "Not found",
-			"PostTime": "Not found"
-		}
-		return info
+		return None
 
 # Returns a list of all posts by the user
 def getPostsByUserID(id):
-	result = db.session.query(model.Posts).filter(model.Posts.UserID == id).all()
+	result = db.session.query(db_models.Posts).filter(db_models.Posts.uid == id).all()
 	if result is not None:
 		return result
 	else:
-		info = {"None found"}
-		return info
+		return None
