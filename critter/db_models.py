907342e4a5db085b3dd121ae3ad09c2b22d4c310
Christopher John <christopherjohn00@protonmail.com>
2022-03-08 08:29:59 -0500

    Aligned comments.

--- critter/db_models.py
+++ critter/db_models.py
@@ -13,21 +13,21 @@ class Users(UserMixin, db.Model):
 
 	__tablename__ = "Users"
 
-	UserID = db.Column(db.Integer, primary_key = True)		#User ID
+	UserID = db.Column(db.Integer, primary_key = True)	#User ID
 	Username = db.Column(db.String(), unique = True)	#Username
 	Email = db.Column(db.String(), unique = True)		#Email
-	Password = db.Column(db.String())				#Password Hash
+	Password = db.Column(db.String())					#Password Hash
 	
 	# Create the relationship between Users and User_Profiles
 	user_profiles = db.relationship('User_Profiles', backref='users', uselist=False)
 
 	#Creates a password hash from a given password.
-	def set_password(self,password):
+	def set_password(self, password):
 		self.Password = generate_password_hash(password)
      
 	#Checks if the given password matches the existing password hash.
-	def check_password(self,password):
-		return check_password_hash(self.Password,password)
+	def check_password(self, password):
+		return check_password_hash(self.Password, password)
 		
 class User_Profiles(db.Model):
 
@@ -59,7 +59,7 @@ class Post_Types(db.Model):
 	__tablename__ = 'Post_Types'
 	
 	TypeID = db.Column(db.Integer, primary_key = True)	#Post Type ID
-	PostType = db.Column(db.String)					#Post Type [ text | photo | video ]
+	PostType = db.Column(db.String)						#Post Type [ text | photo | video ]
 	
 	# Create relationships
 	posts = db.relationship('Posts', backref='types')
@@ -68,12 +68,12 @@ class Posts(db.Model):
 	
 	__tablename__ = 'Posts'
 	
-	PostID = db.Column(db.Integer, primary_key = True)					#Post ID
-	UserID = db.Column(db.Integer, db.ForeignKey(User_Profiles.UserID))	#Author ID
+	PostID = db.Column(db.Integer, primary_key = True)						#Post ID
+	UserID = db.Column(db.Integer, db.ForeignKey(User_Profiles.UserID))		#Author ID
 	PostType = db.Column(db.Integer, db.ForeignKey(Post_Types.PostType))	#Post Type (Type ID)
-	Text = db.Column(db.String)										#Text-Content
+	Text = db.Column(db.String)												#Text-Content
 	MediaID = db.Column(db.Integer, db.ForeignKey(Media.MediaID))			#Media ID
-	PostTime = db.Column(db.DateTime)									#Date/time of post
+	PostTime = db.Column(db.DateTime)										#Date/time of post
 	
 	# Create relationships
 	likes = db.relationship('Likes', backref='postslikes')
@@ -85,7 +85,7 @@ class Likes(db.Model):
 	
 	LikeID = db.Column(db.Integer, primary_key = True)					#Like ID
 	UserID = db.Column(db.Integer, db.ForeignKey(User_Profiles.UserID))	#Liker's ID
-	PostID = db.Column (db.Integer, db.ForeignKey(Posts.PostID))			#Liked Post ID
+	PostID = db.Column (db.Integer, db.ForeignKey(Posts.PostID))		#Liked Post ID
 
 		
 class Follows(db.Model):
@@ -93,5 +93,5 @@ class Follows(db.Model):
 	__tablename__ = 'Follows'
 	
 	RelationshipID = db.Column(db.Integer, primary_key = True)					#Relationship ID
-	UserID = db.Column(db.Integer, db.ForeignKey(User_Profiles.UserID))	#Follower's ID
+	UserID = db.Column(db.Integer, db.ForeignKey(User_Profiles.UserID))			#Follower's ID
 	FollowingID = db.Column(db.Integer, db.ForeignKey(User_Profiles.UserID))	#Followed's ID
